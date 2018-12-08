62421d87d236164897fb93845f51ca2e86314bc4
Emily Ebling <eblinge@etown.edu>
2018-12-07 19:52:17 -0500

    Initial Commit

---
+++ DayOneAOC/src/PartOne.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PartOne {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner(new File("src/input"));
+		
+		int sum = 0;
+		
+		while (in.hasNextLine())
+			sum += in.nextInt();
+
+		System.out.println(sum);
+
+	}
+
+}
---
+++ DayOneAOC/src/PartTwo.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class PartTwo {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner(new File("src/input"));
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		int sum = 0;
+		int index = 0;
+		boolean found = false;
+		
+		while (in.hasNext())
+			list.add(in.nextInt());
+		
+		while (!found) {
+			if (index == list.size())
+				index = 0;
+			if (!set.contains(sum)) {
+				set.add(sum);
+				sum += list.get(index);
+				index++;
+			}
+			else 
+				found = true;
+						
+		}
+
+		System.out.println(sum);
+		
+	}
+		
+}
